ccefc3115e92b0e923f4a785c56f1b9790baac84
Samy Mehdid <s.mahdid@univ-boumerdes.dz>
2023-01-10 16:29:49 +0100

    added routing example app

---
+++ RouterExample/RouterExample/RouterExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouterExampleApp.swift
+//  RouterExample
+//
+//  Created by apple on 10/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct RouterExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
